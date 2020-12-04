b1788803c335de732cbd87617785fbe440221afd
wangrx23694 <1530594251@qq.com>
2020-12-04 10:44:07 +0800

    submit

--- src/main/java/design/strategy/TopN.java
+++ src/main/java/design/strategy/TopN.java
@@ -4,8 +4,7 @@ package design.strategy;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
 /**
  * @author wangrx23694
@@ -21,7 +20,8 @@ public class TopN {
     }
 
     public static void adjust(int[] arr, int i, int end) {
-        int child = (i << 1) + 1; // 当前节点的左孩子
+        // 当前节点的左孩子
+        int child = (i * 2) + 1;
         int temp = arr[i];
 
         while (child <= end) {
@@ -30,9 +30,9 @@ public class TopN {
                 child++;
             }
             if (temp < arr[child]) {
-                arr[i] = arr[child]; // 孩子节点与当前节点替换
+                arr[i] = arr[child];
                 i = child;
-                child = (i << 1) + 1;
+                child = (i * 2) + 1;
             } else {
                 break;
             }
@@ -43,46 +43,48 @@ public class TopN {
     public static void headSort(int[] arr) {
         // 取出堆顶元素，与最后一个元素交换，调整堆
         for (int i = arr.length - 1; i >= 0; i--) {
-            int temp = arr[i]; // 最后一个元素
+            // 最后一个元素
+            int temp = arr[i];
             arr[i] = arr[0];
             arr[0] = temp;
             adjust(arr, 0, i - 1);
-
         }
     }
 
     public static void main(String[] args) {
-        int[] arr = {19, 17, 20, 18, 16, 21};
-        createHeap(arr); // 创建堆
-        headSort(arr);
+        Random random = new Random();
+        int[] arr1 = new int[1000];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = random.nextInt();
+        }
+
+        int[] arr = new int[100];
+        FileReader reader = null;
+        try {
+            reader = new FileReader("../../..");
+            BufferedReader br = new BufferedReader(reader);
+            String line;
+            int i = 0;
+            // 100 0000 0000
+            while ((line = br.readLine()) != null) {
+                int a = Integer.parseInt(line);
+                if (arr.length < 99) {
+                    arr[i] = a;
+                } else if (arr.length == 99) {
+                    createHeap(arr);
+                } else {
+                    // 取更小的值
+                    if (a < arr[0]) {
+                        arr[99] = a;
+                        headSort(arr);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
-        System.out.println();
-
-//        FileReader reader = null;
-//        try {
-//            reader = new FileReader("../../..");
-//            BufferedReader br = new BufferedReader(reader);
-//
-//            String line;
-//            List<Integer> list = new ArrayList<>();
-//            int[] arr = new int[100];
-//            // 100 0000 0000
-//            TopN n = new TopN();
-//            while ((line = br.readLine()) != null) {
-//                int a = Integer.parseInt(line);
-//                if (list.size() < 99) {
-//                    arr[]
-//                } else if (list.size() == 99) {
-//                    cr(100, );
-//                } else {
-//                    adjust();
-//                }
-//            }
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
     }
-
 }
